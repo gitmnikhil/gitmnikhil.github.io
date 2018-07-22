4aa9a0279e012bba034dcd1e28fe9e0909609a36
Nikhil Mittal <nikhilmittalece@gmail.com>
2018-07-22 14:47:34 +0530

    Adding articles

---
+++ articlesList.js
@@ -0,0 +1,21 @@
+var articleList= [
+    {
+        "name":"DIR-3 KYC",
+        "link":"dir3kyc.html"
+    }
+]
+
+//Sample
+/*
+
+    {
+        "name":"article1",
+        "link":"notification.json"
+    },
+    {
+        "name":"category 1",
+        "itemList":[
+
+        ]
+    }
+*/
