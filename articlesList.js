195d097c9afae5c97f9bd50234ad0bb1878a9edc
Nikhil Mittal <nikhilmittalece@gmail.com>
2018-08-07 21:11:28 +0530

    Update articlesList.js

--- articlesList.js
+++ articlesList.js
@@ -2,7 +2,7 @@ var articleList= [
     {
         "name":"DIR-3 KYC",
         "link":"articles/dir3kycs.html"
-    }
+    },
     {
         "name":"GST Registeration",
         "link":"articles/gstregisteration.html"
